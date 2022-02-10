d4b5e5b9ca3fa58b113bdb669db738016b4e8cad
Nikunj Kheni <nikunj_kheni@epam.com>
2022-02-10 12:41:30 +0530

    Initial commit

---
+++ src/main/java/com/gitops/poc/azure/deployment/controller/ApplicationController.java
@@ -0,0 +1,15 @@
+package com.gitops.poc.azure.deployment.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/application")
+public class ApplicationController {
+
+    @GetMapping("/test")
+    public String testApplication() {
+        return "Application is working fine";
+    }
+}

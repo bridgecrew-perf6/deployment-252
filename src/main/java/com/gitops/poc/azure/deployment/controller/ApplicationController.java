5c74e570b1482d8157accd18c571188626aa935b
Nikunj Kheni <nikunj_kheni@epam.com>
2022-02-23 12:29:48 +0530

    Added few lines for Sonar checking

--- src/main/java/com/gitops/poc/azure/deployment/controller/ApplicationController.java
+++ src/main/java/com/gitops/poc/azure/deployment/controller/ApplicationController.java
@@ -4,16 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/application")
 public class ApplicationController {
+
+    private static final String TEST_VARIABLE = "Test Variable";
 
     @GetMapping("/test")
     public String testApplication() {
         return "Application is working fine";
     }
 
-
-   
+    public String testMethod() {
+        return null;
+    }
     
 }
